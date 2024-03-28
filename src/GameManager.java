27413218b239b55ab77c422bb8fda73b7462005c
Ceffrece <cheffrece@quinnipiac.edu>
2024-03-28 15:12:42 -0400

    gamemanager has a little more function

--- src/GameManager.java
+++ src/GameManager.java
@@ -1,13 +1,32 @@
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class GameManager implements Runnable{
+    public static Boolean nextQ;
+    public static int clientAnswering;
     private ConcurrentLinkedQueue queue;
     public GameManager(ConcurrentLinkedQueue q){
         queue = q;
+        nextQ = false;
+        clientAnswering = 0;//"Null" value for clinets -> No client 0
     }
 
     @Override
     public void run() {
+        Boolean answerRecieved = false;
+        while(!answerRecieved){
+            if(nextQ == false){
+                if(queue.isEmpty()){
+                    answerRecieved = false;
+                }
+                if(!queue.isEmpty()){
+                    answerRecieved = true;
+                }
+            }
+        } 
+        //If reached this point, a client has answered and the queue is not empty
+        //Pull first item from the queue
+        //clientAnswering = Item.getClientName();
+        //Clients will look at this then the correct one will answer
         
     }
 }
