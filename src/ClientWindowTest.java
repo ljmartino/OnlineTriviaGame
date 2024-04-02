155aba309569d965b7ceec041cdaa56add74c06c
Ceffrece <cheffrece@quinnipiac.edu>
2024-04-02 14:22:02 -0400

    made booleans in gamemanager work

--- src/ClientWindowTest.java
+++ src/ClientWindowTest.java
@@ -2,7 +2,7 @@ import java.io.FileNotFoundException;
 
 public class ClientWindowTest {
     public static void main(String[] args) throws FileNotFoundException{
-		String ipAddress = "localhost";
+		String ipAddress = "10.111.142.92";
 		int port = 5000;
 		ClientWindow window = new ClientWindow(ipAddress, port);
 	}
--- src/GameManager.java
+++ src/GameManager.java
@@ -1,8 +1,10 @@
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class GameManager implements Runnable{
+    public static Boolean gameIsRunning = true;
     public static Boolean nextQ;
     public static int clientAnswering;
+    public static Boolean clientAnswered;
     private ConcurrentLinkedQueue<Item> queue;
     public GameManager(ConcurrentLinkedQueue<Item> q){
         queue = q;
@@ -12,21 +14,27 @@ public class GameManager implements Runnable{
 
     @Override
     public void run() {
-        Boolean answerReceived = false;
-        while(!answerReceived){
-            if(nextQ == false){
-                if(queue.isEmpty()){
-                    answerReceived = false;
+        while(gameIsRunning){
+            Boolean answerReceived = false;
+            clientAnswered = false;
+            while(!answerReceived){
+                if(nextQ == false){
+                    if(queue.isEmpty()){
+                        answerReceived = false;
+                    }
+                    if(!queue.isEmpty()){
+                        answerReceived = true;
+                    }
                 }
-                if(!queue.isEmpty()){
-                    answerReceived = true;
-                }
+            } 
+            //If reached this point, a client has answered and the queue is not empty
+            //Pull first item from the queue
+            clientAnswering = queue.peek().getID();//Gets ID of first client inside the queue
+            //Clients will look at this then the correct one will answer
+            while(!clientAnswered){
+                //stay
             }
-        } 
-        //If reached this point, a client has answered and the queue is not empty
-        //Pull first item from the queue
-        clientAnswering = queue.peek().getID();//Gets ID of first client inside the queue
-        //Clients will look at this then the correct one will answer
-        
+            nextQ = true;
+        }
     }
 }
