b01fd7fd9d331e88526e5b0d7cb42cbd8dbcdc2d
PeterZegarek <petrzegarek@gmail.com>
2024-04-04 16:03:52 -0400

    Join period 30 seconds

--- src/MainServer.java
+++ src/MainServer.java
@@ -43,7 +43,7 @@ public class MainServer{
         
         System.out.println("Joining period has started, 30 seconds to join");  //Does this work?????
         try {
-            Thread.sleep(3000); //Waits 30 seconds to start accepting
+            Thread.sleep(30000); //Waits 30 seconds to start accepting
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
